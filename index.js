40c14f05d977eeb3bfd15fad7d7727c789051553
Kai Laptop <kaijohan428@gmail.com>
2019-07-23 19:18:18 +0200

    counts amount of documents that match filter

--- index.js
+++ index.js
@@ -43,13 +43,10 @@ async function createCourse() {
 
 async function getCourses() {
     const courses = await Course
-        // .find({ author: 'Mosh', isPublished: true})
-        .find({ author: /^Mosh/ }) // string starts with Mosh
-        .find({ author: /Surname$/i }) // string ends with Surname (the i makes it case insensitive)
-        .find({ author: /.*Mosh.*/i}) // contains Mosh
+        .find({ author: 'Mosh', isPublished: true})
         .limit(10)
         .sort({ name: 1 })
-        .select({ name: 1, tags: 1 });
+        .count();
     console.log(courses);
 }
 
